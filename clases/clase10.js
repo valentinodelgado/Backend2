34b0eab8d85013ba00d72378ef31ee6ae8edf8da
Valentino Delgado <valentinol.delgado@gmail.com>
2024-09-03 11:47:59 -0300

    Añadir la carpeta 'clases' al .gitignore

--- clases/clase10.js
+++
@@ -1,9 +0,0 @@
-//server size rendering (ssr): 
-
-//Motores de plantilla: procesa informacion y la brinda del servidor al cliente
-
-//handlebars: esta pensado para un dinamismo de nivel medio (blog), procesa el html que queremos renderizar, no esta pensado para elemntos de cambios constantes
-
-//el main renderiza el resto de handlebars y se lo manda al cliente
-
-//si queremos utilizar uuna estructura como un condicional o un iterable utilizamos # ejemplo {{#if condicion}}
--- clases/clase15.js
+++
@@ -1,3 +0,0 @@
-//Mongoose
-//Mongoose es un ODM nos permitira definir esquemas para poder gestionar colecciones y documentos
-//Una vez que comprendemos que MongoDB permite gestionar documentos similar a un objeto. Además, mongoose nos permitirá conectar con la base de datos gestionada desde Mongo Atlas, con el fin de poder mantener la gestión de la base de datos desde la nube.
